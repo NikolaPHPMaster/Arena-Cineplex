9dd150e4fbf956b39353d11bd9d498d1027074ed
NIKOLA MARKOVIC <pomocnih@gmail.com>
2021-03-02 23:10:00 +0100

    Add project files.

---
+++ Restoran/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restoran.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Dogadjaji()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+         public ActionResult Galerija()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
